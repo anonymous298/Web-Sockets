1599a619ca50d320ab6531aa9388d9e3f14e3602
anonymous298 <tackletalha@gmail.com>
2025-09-15 16:42:46 +0500

    c

---
+++ test.py
@@ -0,0 +1,26 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+client = OpenAI(
+    base_url="https://openrouter.ai/api/v1",
+    api_key=os.getenv('API')
+)
+
+completion = client.chat.completions.create(
+  extra_headers={
+    "HTTP-Referer": "<YOUR_SITE_URL>", # Optional. Site URL for rankings on openrouter.ai.
+    "X-Title": "<YOUR_SITE_NAME>", # Optional. Site title for rankings on openrouter.ai.
+  },
+  extra_body={},
+  model="deepseek/deepseek-r1-0528:free",
+  messages=[
+    {
+      "role": "user",
+      "content": "How, to build Full Stack Saas"
+    }
+  ]
+)
+print(completion.choices[0].message.content)
